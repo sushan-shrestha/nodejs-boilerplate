f5cb04a1ad70c3a427c3746714c15abc7a64d909
Sushan <sushan.shrestha10@gmail.com>
2022-12-22 13:05:30 +0545

    add routes to health-check

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import userRoutes from './routes/userRoutes';
-import authRoutes from './routes/authRoutes';
-
+import userRoutes from "./routes/userRoutes";
+import authRoutes from "./routes/authRoutes";
 
 /**
  * Contains all API routes for the application.
@@ -12,16 +11,29 @@ const router = Router();
 /**
  * GET /api
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.json({
     app: req.app.locals.title,
     apiVersion: req.app.locals.version,
   });
-
-
 });
 
-router.use('/users', userRoutes);
-router.use('/auth', authRoutes);
+/**
+ * GET /api/health
+ */
+router.get("/health-check", (req, res) => {
+  const healthcheck = {
+    message: "OK",
+  };
+  try {
+    res.send(healthcheck);
+  } catch (e) {
+    healthcheck.message = e;
+    res.status(503).send();
+  }
+});
+
+router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
